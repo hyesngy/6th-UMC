e40b72096d24544fa572624e081be7222456a151
hyesngy <tell280210@gmail.com>
2024-07-02 17:42:51 +0900

    🐛 week10 Fix RedirectURI

--- week10/mission/src/RedirectURI.jsx
+++ week10/mission/src/RedirectURI.jsx
@@ -2,9 +2,9 @@ const getRedirectURI = () => {
     const hostname = window.location.hostname;
 
     return hostname === "localhost" ? "http://localhost:5173/login/auth"
-    : hostname === "hansung-kakao-login.netlify.app" ? "https://hansung-kakao-login.netlify.app/login/auth"
-    : hostname === "main--hansung-kakao-login.netlify.app" ? "https://main--hansung-kakao-login.netlify.app/login/auth"
-    : "https://default-url.com/login/auth";
+    : hostname === "umc-week10.netlify.app" ? "https://umc-week10.netlify.app/login/auth"
+    : hostname === "main--umc-week10.netlify.app" ? "https://main--umc-week10.netlify.app/login/auth"
+    : "https://umc-week10.netlify.app/login/auth";
     
   };
   
